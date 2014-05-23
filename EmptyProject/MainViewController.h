218bcf2ab32b96b59a034fab65fd39cf380ce2df
Dirk de Boer <dirk@buzzar.net>
2014-05-23 11:00:44 +0200

    Initial commit
    First commit

---
+++ EmptyProject/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewViewController.h
+//  EmptyProject
+//
+//  Created by Stofkat.org on 23-05-14.
+//  Copyright (c) 2014 Stofkat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ EmptyProject/MainViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewViewController.m
+//  EmptyProject
+//  An empty project for Xcode that does not use the Storyboard interface, but just good old ViewControllers
+//  From Xcode 5.0 and up the project is default set to using Storyboard and you can no longer choose not to use them
+//  it requires some time to change it back, so that's where this default project might come in handy
+//  Created by Stofkat.org on 23-05-14.
+//  Copyright (c) 2014 Stofkat. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
